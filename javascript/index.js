19988d239f87ee1084fcc558e19febc01b1ea7b8
SCPassion <141584825+SCPassion@users.noreply.github.com>
2024-12-08 21:38:25 +0100

    Added hide-condition & JS

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+const hideBlogs = document.getElementsByClassName('hide')
+
+document.addEventListener('click', (e)=> {
+    if(e.target.id=="view-more") {
+        console.log("view more!")
+    }
+})
